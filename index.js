b407b6dc68ee7e5c1c70179b525fa925a6b19572
LAPTOP-4QR8R40O\asus <kelviantoh30@gmail.com>
2020-11-21 20:11:31 +0700

    first deploy to heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('Hello, World!')
+    res.end('Hello World\n');
 })
 
 app.listen(port, () => console.log(`Server started on Port ${port}`));
