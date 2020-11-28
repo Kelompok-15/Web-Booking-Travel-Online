fa8df46da80c37d1924af4f0758c21bcb001a6d4
Sandy Danish <70734332+Sandydanish@users.noreply.github.com>
2020-11-28 20:14:04 +0700

    Add new routes

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ app.use( express.static( "public" ) );
 
 const indexRouter = require('./routes/index');
 const reccoRouter = require('./routes/recommendation');
+const hotelRouter = require('./routes/hotel');
+const planeRouter = require('./routes/plane');
+const trainRouter = require('./routes/train');
 
 app.use('/', indexRouter);
 app.use('/recommendation', reccoRouter);
+app.use('/hotel', hotelRouter);
+app.use('/plane', planeRouter);
+app.use('/train', trainRouter);
 
 app.listen(port, () => console.log(`Server started on Port ${port}`));
