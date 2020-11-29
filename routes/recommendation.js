9906543bc5ffdd9c9f056f9c5d59f861f7e7e72c
LAPTOP-4QR8R40O\asus <kelviantoh30@gmail.com>
2020-11-29 16:17:18 +0700

    add files

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -9,4 +9,42 @@ router.get('/', async (req, res) => {
 router.get("/bora", async (req, res) => {
    res.render('pages/bora');
 })
+
+router.get("/sydney", async (req, res) => {
+   res.render('pages/sydney');
+})
+
+router.get("/maui", async (req, res) => {
+   res.render('pages/maui');
+})
+
+router.get("/rajaampat", async (req, res) => {
+   res.render('pages/rajaampat');
+})
+
+router.get("/paris", async (req, res) => {
+   res.render('pages/paris');
+})
+
+router.get("/montana", async (req, res) => {
+   res.render('pages/montana');
+})
+
+router.get("/seoul", async (req, res) => {
+   res.render('pages/seoul');
+})
+
+router.get("/swiss", async (req, res) => {
+   res.render('pages/swiss');
+})
+
+router.get("/tokyo", async (req, res) => {
+   res.render('pages/tokyo');
+})
+
+router.get("/budapest", async (req, res) => {
+   res.render('pages/budapest');
+})
+
+
 module.exports = router;
