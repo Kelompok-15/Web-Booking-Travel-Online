d8491d3f97ef6391025caa794a2f38fd8b8897ed
Mega Karina <70811667+megakarina@users.noreply.github.com>
2020-12-03 17:20:19 +0700

    add files signin, signup, and about us

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use( express.static( "public" ) );
+
+const indexRouter = require('./routes/index');
+const reccoRouter = require('./routes/recommendation');
+const hotelRouter = require('./routes/hotel');
+const planeRouter = require('./routes/plane');
+const trainRouter = require('./routes/train');
+const signinRouter = require('./routes/signin');
+const signupRouter = require('./routes/signup');
+const aboutusRouter = require('./routes/aboutus');
+
+app.use('/', indexRouter);
+app.use('/recommendation', reccoRouter);
+app.use('/hotel', hotelRouter);
+app.use('/plane', planeRouter);
+app.use('/train', trainRouter);
+app.use('/signin', signinRouter);
+app.use('/signup', signupRouter);
+app.use('/aboutus', aboutusRouter);
+
+app.listen(port, () => console.log(`Server started on Port ${port}`));
