93fac436b5653baa91c73ffea7baec744aa47779
LAPTOP-4QR8R40O\asus <kelviantoh30@gmail.com>
2020-12-08 17:18:30 +0700

    fix bugs

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use( express.static( "public" ) );
 
 const indexRouter = require('./routes/index');
 const reccoRouter = require('./routes/recommendation');
-const hotelRouter = require('./routes/hotel');
-const planeRouter = require('./routes/plane');
-const trainRouter = require('./routes/train');
+//const hotelRouter = require('./routes/hotel');
+//const planeRouter = require('./routes/plane');
+//const trainRouter = require('./routes/train');
 const signinRouter = require('./routes/signin');
 const signupRouter = require('./routes/signup');
 const aboutusRouter = require('./routes/aboutus');
@@ -19,9 +19,9 @@ const packageRouter = require('./routes/package');
 
 app.use('/', indexRouter);
 app.use('/recommendation', reccoRouter);
-app.use('/hotel', hotelRouter);
-app.use('/plane', planeRouter);
-app.use('/train', trainRouter);
+//app.use('/hotel', hotelRouter);
+//app.use('/plane', planeRouter);
+//app.use('/train', trainRouter);
 app.use('/signin', signinRouter);
 app.use('/signup', signupRouter);
 app.use('/aboutus', aboutusRouter);
